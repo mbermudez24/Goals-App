97c184e83400177b0b389f474dbd050bc4802ca9
mbermudez24 <mabermudez87952@umanizales.edu.co>
2024-01-19 19:25:25 -0500

    Modified App.js with my new components and added tailwind to the index.css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 
 import './App.css';
+import Footer from './components/shared/Footer';
+import Header from './components/shared/Header';
+import Principal from './components/shared/Principal';
 
 function App() {
   return (
     <div className="App">
-
+      <Header></Header>
+      <Principal></Principal>
+      <Footer></Footer>
     </div>
   );
 }
