a223489ed30d2b1dfd8240eb10d01af4d36de4ae
mbermudez24 <mabermudez87952@umanizales.edu.co>
2024-01-20 14:36:15 -0500

    CSS for goal form (Details.js)

--- src/components/new/Details.js
+++ src/components/new/Details.js
@@ -1,44 +1,58 @@
+import styles from "./Details.module.css"
+
 function Details() {
     const frequencyoptions = ["day", "week", "month", "year"];
     const Iconslist = ["💻", "🏃‍♂️", "📚", "✈️", "💵"];
     return (
-        <div>
-            <form action="">
-                <label htmlFor="">
+        <div className="card">
+            <form action="" className="p-4">
+                <label htmlFor="" className="label">
                     Describe your goal
-                    <input placeholder="eg. 52 walks" />
+                    <input
+                        className="input"
+                        placeholder="eg. 52 walks"
+                    />
                 </label>
-                <label htmlFor="">
+                <label htmlFor="" className="label">
                     How often do you want to meet your goal? <span>(eg. once a week)</span>
-                    <div>
-                        <input type="number" />
-                        <select name="" id="">
+                    <div className="flex mb-6">
+                        <input
+                            className="input mr-6"
+                            type="number"
+                        />
+                        <select name="" id="" className="input">
                             {frequencyoptions.map(option => <option value={option}>{option}</option>)}
                         </select>
                     </div>
                 </label>
-                <label htmlFor="">
+                <label htmlFor="" className="label">
                     How many times do you want to complete this goal?
-                    <input type="number" />
+                    <input
+                        className="input"
+                        type="number" />
                 </label>
-                <label htmlFor="">
+                <label htmlFor="" className="label">
                     Do you have a deadline?
-                    <input type="date" />
+                    <input
+                        className="input"
+                        type="date" />
                 </label>
-                <label htmlFor="">
+                <label htmlFor="" className="label">
                     How many times have you already completed this goal?
-                    <input type="number" />
+                    <input
+                        className="input"
+                        type="number" />
                 </label>
-                <label htmlFor="">
+                <label htmlFor="" className="label">
                     Choose the icon for the goal
-                    <select name="" id="">
+                    <select name="" id="" className="input">
                         {Iconslist.map(icon => <option value={icon}>{icon}</option>)}
                     </select>
                 </label>
             </form>
-            <div>
-                <button>Create</button>
-                <button>Cancel</button>
+            <div className={styles.botones}>
+                <button className="boton boton--negro">Create</button>
+                <button className="boton boton--gris">Cancel</button>
             </div>
         </div>
     );
