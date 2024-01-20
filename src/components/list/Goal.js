c996196323e5a7ed17ac45311a5f32f0e56cbd2f
mbermudez24 <mabermudez87952@umanizales.edu.co>
2024-01-19 22:50:56 -0500

    Added a neumorphism plugin to tailwind and created the html for the goal component

---
+++ src/components/list/Goal.js
@@ -0,0 +1,25 @@
+function Goal() {
+    return (
+        <div>
+            <div>
+                <div>icon</div>
+                <div>
+                    <p>1<sub>/ week</sub></p>
+                    <p>Details</p>
+                </div>
+            </div>
+            <div>
+                <div>
+                    <p>4 of 5</p>
+                    <div>
+                        <div></div>
+                    </div>
+                </div>
+                <button>Completed</button>
+
+            </div>
+        </div>
+    );
+}
+
+export default Goal;
